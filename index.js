b4567048117aa9d79fc4faf59a3083ed3320cbf7
Edilberto Ruvalcaba <eruvalcaba@linkedin.com>
2016-06-18 17:29:56 -0700

    Read configuration from envioronment file or coverage-config

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var Funnel = require('broccoli-funnel');
 var BroccoliMergeTrees = require('broccoli-merge-trees');
 var CoverageInstrumenter = require('./lib/coverage-instrumenter');
 var attachMiddleware = require('./lib/attach-middleware');
-var config = require('./lib/config');
+var getConfig = require('./lib/config');
 
 module.exports = {
   name: 'ember-cli-code-coverage',
@@ -22,26 +22,32 @@ module.exports = {
   postprocessTree: function(type, tree) {
     if (this._coverageIsEnabled() && type === 'js') {
       var appFiles = new Funnel(tree, {
-        exclude: this._config().excludes
+        exclude: this.project.config(this.app.env).excludes
       });
       var instrumentedNode = new CoverageInstrumenter(appFiles, {annotation: 'Instrumenting for code coverage'});
       return new BroccoliMergeTrees([tree, instrumentedNode], { overwrite: true });
     }
     return tree;
   },
+
   testemMiddleware: function(app) {
     if (!this._coverageIsEnabled()) { return; }
-    attachMiddleware(app, { root: this.project.root, ui: this.ui });
+    var config = this.project.config(this.app.env);
+    attachMiddleware(app, this.project.root, config.coverageFolder, config.reporters);
   },
+
   serverMiddleware: function(options) {
     if (!this._coverageIsEnabled()) { return; }
-    attachMiddleware(options.app, { root: this.project.root, ui: this.ui });
+    var config = this.project.config(this.app.env);
+    attachMiddleware(options.app, this.project.root, config.coverageFolder, config.reporters);
   },
+
+  config: function(env, appConfig) {
+    return getConfig(this.project.root, appConfig);
+  },
+
   _coverageIsEnabled: function() {
-    return process.env[this._config().coverageEnvVar];
-  },
-  _config: function() {
-    return config(this.project.root);
+    return process.env[this.project.config(this.app.env).coverageEnvVar];
   }
 };
 
